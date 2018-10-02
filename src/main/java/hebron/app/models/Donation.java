22b7fa54760bdde25e4d4f9dbd7f74659355f5d3
DimaUA99 <kachanovskiyfant.99@gmail.com>
2018-10-02 16:27:36 +0300

    make entity class

---
+++ src/main/java/hebron/app/models/Donation.java
@@ -0,0 +1,28 @@
+package hebron.app.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Double summ;
+
+    private Date date;
+
+    private Long projectId;
+
+    private Contributor contributor;
+}
